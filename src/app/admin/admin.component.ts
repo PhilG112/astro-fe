c197ac91f9c1835cf74c2837586fb5f1bad5a5d0
Philg112 <phil.grujovski@gmail.com>
2019-09-10 22:52:15 +1000

    Update admin

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss']
 })
-export class AdminComponent {
+export class AdminComponent implements OnInit, OnDestroy {
+  private element = <HTMLElement>document.getElementsByClassName('navbar')[0];
+
   constructor() {
+  }
+
+  ngOnInit(): void {
+    this.element.style.display = 'none';
+  }
+
+  ngOnDestroy(): void {
+    this.element.style.display = 'block';
   }
 }
 
